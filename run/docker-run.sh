06246832410d4d2724232a75e1f0235ee9efbcda
Francis Gagnaire <fgagnaire@kcg.com>
2017-07-11 15:31:14 -0500

    add docker-run.sh

---
+++ run/docker-run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+if [ -z "${DOCKER_NAME}" ];
+then
+    NAME_OPTION="";
+else
+    NAME_OPTION="--name ${DOCKER_NAME}";
+fi
+
+
+HERE=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+RANDOM_ID=$(${HERE}/generate-random-string.sh 5)
+CONTAINER_ID_FILE=/tmp/docker-container-id.${RANDOM_ID}
+
+set +e
+docker run \
+    -it \
+    --rm \
+    --cidfile=${CONTAINER_ID_FILE} \
+    -u $(id -u) \
+    -w ${PWD} \
+    -v ${HOME}:${HOME} \
+    -e XAUTHORITY=:${HOME}/.Xauthority \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp:/tmp \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    --net=host \
+    ${NAME_OPTION} \
+    $@
+
+EXIT_CODE=$?
+CONTAINER_ID=$(cat ${CONTAINER_ID_FILE})
+rm ${CONTAINER_ID_FILE}
+
+exit ${EXIT_CODE}
+
---
+++ run/docker-run.wireshark.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "docker-run.sh fgagnaire/centos73-wireshark-build:latest $@"
+docker-run.sh fgagnaire/centos73-wireshark-build:latest $@
+
